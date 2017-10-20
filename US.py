c7aadebad2a11a3e71a457b244599c0e90d367da
NuclearWinter22 <wilsonja@bearsmail.org>
2017-10-20 14:33:52 -0400

    Update US.py

--- US.py
+++ US.py
@@ -5,9 +5,9 @@ class US:
     def __init__(self, trig, echo):
         self.trig = trig
         self.echo = echo
-        this.readings = [128, 128, 128, 128, 128]
+        self.readings = [128, 128, 128, 128, 128]
 
-    def get_distance(self):
+    def get_distance():
         distance = read_ultrasonic_sensor(trig, echo)
         if (distance != null):
             readings.append(distance)
